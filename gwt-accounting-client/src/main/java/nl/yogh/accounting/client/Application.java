a09e32b991ce999eb26a5e4cbcd7900dd4f4473e
Jorn C <jorncruijsen@gmail.com>
2015-11-30 20:56:19 +0100

    Added shared project and did basic stub UI

--- gwt-accounting-client/src/main/java/nl/yogh/accounting/client/Application.java
+++ gwt-accounting-client/src/main/java/nl/yogh/accounting/client/Application.java
@@ -4,11 +4,14 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
 
 import nl.yogh.accounting.client.di.ApplicationGinjector;
+import nl.yogh.accounting.client.resources.R;
 
 public class Application implements EntryPoint {
   @Override
   public void onModuleLoad() {
     ApplicationGinjector.INSTANCE.inject(this);
+
+    R.init();
 
     RootPanel.get().add(ApplicationGinjector.INSTANCE.getApplicationRootView());
   }
